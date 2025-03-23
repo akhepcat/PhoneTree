3c6fc2532a624f99553b169ab9bda3d36d12cb96
Leif Sawyer <leif@akhepcat.com>
2025-03-23 00:27:46 -0800

    fixups to allow for pgsql

--- src/Includes/Common.pm
+++ src/Includes/Common.pm
@@ -2,14 +2,23 @@
 package Common;
 use Exporter; # 'import';
 
-our @EXPORT = qw($VERSION $DEBUG $USE_SQLITE $dbfile dprint );
+our @EXPORT = qw($VERSION $DEBUG $USE_SQLITE $sqlfile $pgdb $pguser $pgpass dprint );
 
 our $VERSION     = '1.00';
 
 our $DEBUG=1;
 
 our $USE_SQLITE=1;
-our $dbfile="/srv/http/www.akhepcat/db/status.sql";
+
+if ($USE_SQLITE == 1) {
+	# use a local sqlite3 file-based db
+	our $sqlfile="/srv/http/www.akhepcat/db/status.sql";
+} else {
+	# use Postgres
+	our $pgdb="dbname=MyPgDB;host=MyPgHost;port=5432;options=''";
+	our $pguser="";
+	our $pgpass="";
+}
 
 sub dprint(@) {
 	my @out=shift;
